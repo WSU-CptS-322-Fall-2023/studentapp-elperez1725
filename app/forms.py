72127eaea4d694191078e2962a63a2ddb0348743
elperez1725 <elliott.perezramos@wsu.edu>
2023-08-31 15:05:42 -0700

    Version v.03 created a new file called create class and styled that file. added a link to get to the file and added a flash container that displays that the class was added succesfully

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,19 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import  ValidationError, Length
+from wtforms.validators import  ValidationError, Length, DataRequired
+from wtforms_sqlalchemy.fields import QuerySelectField
 from app.models import Class, Major
+
+
+def get_major():
+    return Major.query.all()
+
+
+def get_major_label(theMajor):
+    return theMajor.name
 
 class ClassForm(FlaskForm):
     coursenum = StringField('Course Number',[Length(min=3, max=3)])
+    title = StringField("Course Title", validators=[DataRequired()])
+    major = QuerySelectField('Majors', query_factory=get_major, get_label=get_major_label, allow_blank=False)
     submit = SubmitField('Post')
