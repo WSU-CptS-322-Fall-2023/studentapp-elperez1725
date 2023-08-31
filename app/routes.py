7d0966116e6e79b14847c8cc36f4dfb635cb9095
elperez1725 <elliott.perezramos@wsu.edu>
2023-08-30 17:32:25 -0700

    Version v.01 - made new model names Major, changed some inputs for Class model

--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,15 @@ from flask import render_template, flash, redirect, url_for, request
 from app import app,db
 
 from app.forms import ClassForm
-from app.models import Class
+from app.models import Class, Major
+
+
 
 @app.before_request
 def initDB(*args, **kwargs):
     if app.got_first_request:
         db.create_all()
+ 
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
--- pythonStatements.py
+++ pythonStatements.py
@@ -4,15 +4,29 @@ from app import db
 #create the database file, if it doesn't exist. 
 db.create_all()
 
+
+
 # import db models
 from app.models import Class
+from app.models import Major
+
+
 
 #create class objects and write them to the database
-newClass = Class(coursenum='322')
+newClass = Class(coursenum='322', major ="CptS", title="Software Engineering")
 db.session.add(newClass)
-newClass = Class(coursenum='355')
+newClass = Class(coursenum='315', major="CE", title="Fluid Mechanics")
 db.session.add(newClass)
 db.session.commit()
+
+#Creating major objects
+newMajor = Major(name='Cpts', department="School of EECS")
+db.session.add(newMajor)
+newMajor = Major(name="CE", department="Civil Engineering")
+db.session.add(newMajor)
+Major.query.all()
+for m in Major.query.all():
+    print(m)
 
 # query and print classes
 Class.query.all()
